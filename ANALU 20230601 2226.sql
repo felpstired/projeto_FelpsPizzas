5a6d6c49a484d3f29b51f1b26db4e735484f42e2
felpstired <analucabbage@gmail.com>
2023-06-05 22:29:13 -0300

    Atualização Banco de Dados

--- ANALU 20230601 2226.sql
+++ ANALU 20230601 2226.sql
@@ -1789,13 +1789,18 @@ CREATE TABLE `tbavaliacoes` (
   PRIMARY KEY (`idavaliacoes`,`idpessoas`) USING BTREE,
   KEY `FK_idpessoas_idavaliacoes` (`idpessoas`),
   CONSTRAINT `FK_idpessoas_idavaliacoes` FOREIGN KEY (`idpessoas`) REFERENCES `tbpessoas` (`idpessoas`) ON DELETE NO ACTION ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tbavaliacoes`
 --
 
 /*!40000 ALTER TABLE `tbavaliacoes` DISABLE KEYS */;
+INSERT INTO `tbavaliacoes` (`idavaliacoes`,`idpessoas`,`comentario`,`profissao`,`estrelas`,`imagem`,`cadastro`,`alteracao`,`ativo`) VALUES 
+ (1,1,'Proin iaculis purus consequat sem cure digni ssim donec porttitora entum suscipit rhoncus. Accusantium quam, ultricies eget id, aliquam eget nibh et. Maecen aliquam, risus at semper.','Ceo &amp; Founder',1,'assets/img/testimonials/testimonials-1.jpg','2023-04-28 12:35:43','2023-06-05 22:20:34','A'),
+ (2,2,'Export tempor illum tamen malis malis eram quae irure esse labore quem cillum quid cillum eram malis quorum velit fore eram velit sunt aliqua noster fugiat irure amet legam anim culpa.','Ceo &amp; Founder',4,'assets/img/testimonials/testimonials-2.jpg','2023-04-28 12:35:43','2023-06-05 22:14:02','A'),
+ (3,3,'Enim nisi quem export duis labore cillum quae magna enim sint quorum nulla quem veniam duis minim tempor labore quem eram duis noster aute amet eram fore quis sint minim.','Ceo &amp; Founder',3,'assets/img/testimonials/testimonials-3.jpg','2023-04-28 12:35:43','2023-06-05 22:14:02','A'),
+ (4,4,'Quis quorum aliqua sint quem legam fore sunt eram irure aliqua veniam tempor noster veniam enim culpa labore duis sunt culpa nulla illum cillum fugiat legam esse veniam culpa fore nisi cillum quid.','Ceo &amp; Founder',5,'assets/img/testimonials/testimonials-4.jpg','2023-04-28 12:35:43','2023-06-05 22:14:02','A');
 /*!40000 ALTER TABLE `tbavaliacoes` ENABLE KEYS */;
 
 
@@ -1814,7 +1819,7 @@ CREATE TABLE `tbbanner` (
   `alteracao` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `ativo` char(1) NOT NULL DEFAULT 'A',
   PRIMARY KEY (`idbanner`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tbbanner`
@@ -1822,7 +1827,8 @@ CREATE TABLE `tbbanner` (
 
 /*!40000 ALTER TABLE `tbbanner` DISABLE KEYS */;
 INSERT INTO `tbbanner` (`idbanner`,`imagem`,`titulo`,`descricao`,`video`,`cadastro`,`alteracao`,`ativo`) VALUES 
- (1,'https://i.pinimg.com/originals/e6/97/d9/e697d97e0496c5b50f3e17d70f6b1745.jpg','Golden Maknae','Integrante mais novo do grupo popular de K-POP, BTS, Jeon Jungkook é o chamado Golden Maknae por ser exepcional em tudo que faz, sendo reconhecido tanto dentro quanto fora da bolha.','https://www.youtube.com/watch?v=djV11Xbc914','2023-04-28 00:00:00','2023-06-01 21:23:46','A');
+ (1,'https://i.pinimg.com/originals/e6/97/d9/e697d97e0496c5b50f3e17d70f6b1745.jpg','Golden Maknae','Integrante mais novo do grupo popular de K-POP, BTS, Jeon Jungkook é o chamado Golden Maknae por ser exepcional em tudo que faz, sendo reconhecido tanto dentro quanto fora da bolha.','https://www.youtube.com/watch?v=djV11Xbc914','2023-04-28 00:00:00','2023-06-02 18:41:05','D'),
+ (2,'assets/img/hero-img.png','Aproveite uma Saudável e<br>Deliciosa Comida','Sed autem laudantium dolores. Voluptatem itaque ea consequatur eveniet. Eum quas beatae cumque eum quaerat.','https://www.youtube.com/watch?v=LXb3EKWsInQ','2023-05-13 23:21:46','2023-06-02 19:20:59','A');
 /*!40000 ALTER TABLE `tbbanner` ENABLE KEYS */;
 
 
@@ -1840,13 +1846,18 @@ CREATE TABLE `tbcard` (
   `alteracao` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `ativo` char(1) NOT NULL DEFAULT 'A',
   PRIMARY KEY (`idcard`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tbcard`
 --
 
 /*!40000 ALTER TABLE `tbcard` DISABLE KEYS */;
+INSERT INTO `tbcard` (`idcard`,`icon`,`titulo`,`descricao`,`cadastro`,`alteracao`,`ativo`) VALUES 
+ (1,'','Por que escolher nosso restaurante?','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit\r\n                Asperiores dolores sed et. Tenetur quia eos. Autem tempore quibusdam vel necessitatibus optio ad corporis.','2023-05-13 12:25:43','2023-06-02 19:43:21','A'),
+ (2,'bi bi-clipboard-data','Corporis voluptates officia eiusmod','Consequuntur sunt aut quasi enim aliquam quae harum pariatur laboris nisi ut aliquip','2023-05-13 12:25:43','2023-06-02 19:32:28','A'),
+ (3,'bi bi-gem','Ullamco laboris ladore pan','Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt','2023-05-13 12:25:43','2023-06-02 19:32:30','A'),
+ (4,'bi bi-inboxes','Labore consequatur incidid dolore','Aut suscipit aut cum nemo deleniti aut omnis. Doloribus ut maiores omnis facere','2023-05-13 12:25:43','2023-06-02 19:32:33','A');
 /*!40000 ALTER TABLE `tbcard` ENABLE KEYS */;
 
 
@@ -1868,13 +1879,26 @@ CREATE TABLE `tbcardapio` (
   PRIMARY KEY (`idcardapio`,`idmenucardapio`) USING BTREE,
   KEY `FK_idmenucardapio_idcardapio` (`idmenucardapio`),
   CONSTRAINT `FK_idmenucardapio_idcardapio` FOREIGN KEY (`idmenucardapio`) REFERENCES `tbmenucardapio` (`idmenucardapio`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tbcardapio`
 --
 
 /*!40000 ALTER TABLE `tbcardapio` DISABLE KEYS */;
+INSERT INTO `tbcardapio` (`idcardapio`,`idmenucardapio`,`imagem`,`tituloimg`,`descimg`,`preco`,`cadastro`,`alteracao`,`ativo`) VALUES 
+ (1,1,'assets/img/menu/menu-item-1.png','Magnam Tiste','Lorem, deren, trataro, filede, nerada',5.95,'2023-04-28 12:35:43','2023-06-05 22:14:02','A'),
+ (2,1,'assets/img/menu/menu-item-2.png','Aut Luia','Lorem, deren, trataro, filede, nerada',14.95,'2023-04-28 12:35:43','2023-06-05 22:18:33','A'),
+ (3,1,'assets/img/menu/menu-item-3.png','Est Eligendi','Lorem, deren, trataro, filede, nerada',8.95,'2023-04-28 12:35:43','2023-06-05 22:18:33','A'),
+ (4,2,'assets/img/menu/menu-item-4.png','Eos Luibusdam','Lorem, deren, trataro, filede, nerada',10.95,'2023-04-28 12:35:43','2023-06-05 22:14:02','A'),
+ (5,2,'assets/img/menu/menu-item-5.png','Luibusdam Eos','Lorem, deren, trataro, filede, nerada',12.95,'2023-04-28 12:35:43','2023-06-05 22:18:33','A'),
+ (6,2,'assets/img/menu/menu-item-6.png','Laboriosam Direva','Lorem, deren, trataro, filede, nerada',9.95,'2023-04-28 12:35:43','2023-06-05 22:18:33','A'),
+ (7,3,'assets/img/menu/menu-item-1.png','Magnam Tiste','Lorem, deren, trataro, filede, nerada',5.95,'2023-04-28 12:35:43','2023-06-05 22:18:33','A'),
+ (8,3,'assets/img/menu/menu-item-2.png','Aut Luia','Lorem, deren, trataro, filede, nerada',14.95,'2023-04-28 12:35:43','2023-06-05 22:14:02','A'),
+ (9,3,'assets/img/menu/menu-item-3.png','Est Eligendi','Lorem, deren, trataro, filede, nerada',8.95,'2023-04-28 12:35:43','2023-06-05 22:18:33','A'),
+ (10,4,'assets/img/menu/menu-item-4.png','Eos Luibusdam','Lorem, deren, trataro, filede, nerada',10.95,'2023-04-28 12:35:43','2023-06-05 22:18:33','A'),
+ (11,4,'assets/img/menu/menu-item-5.png','Luibusdam Eos','Lorem, deren, trataro, filede, nerada',12.95,'2023-04-28 12:35:43','2023-06-05 22:16:06','A'),
+ (12,4,'assets/img/menu/menu-item-6.png','Laboriosam Direva','Lorem, deren, trataro, filede, nerada',9.95,'2023-04-28 12:35:43','2023-06-05 22:18:33','A');
 /*!40000 ALTER TABLE `tbcardapio` ENABLE KEYS */;
 
 
@@ -1965,13 +1989,17 @@ CREATE TABLE `tbeventos` (
   `alteracao` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `ativo` char(1) NOT NULL DEFAULT 'A',
   PRIMARY KEY (`ideventos`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tbeventos`
 --
 
 /*!40000 ALTER TABLE `tbeventos` DISABLE KEYS */;
+INSERT INTO `tbeventos` (`ideventos`,`titulo`,`preco`,`descricao`,`imagem`,`cadastro`,`alteracao`,`ativo`) VALUES 
+ (1,'Festas Customizadas',99,'Quo corporis voluptas ea ad. Consectetur inventore sapiente ipsum voluptas eos omnis facere. Enim facilis veritatis id est rem repudiandae nulla expedita quas.','assets/img/events-1.jpg','2023-04-28 12:35:43','2023-06-05 22:14:02','A'),
+ (2,'Festas Privadas',289,'In delectus sint qui et enim. Et ab repudiandae inventore quaerat doloribus. Facere nemo vero est ut dolores ea assumenda et. Delectus saepe accusamus aspernatur.','assets/img/events-2.jpg','2023-04-28 12:35:43','2023-06-05 22:14:02','A'),
+ (3,'Festas de Aniversário',499,'Laborum aperiam atque omnis minus omnis est qui assumenda quos. Quis id sit quibusdam. Esse quisquam ducimus officia ipsum ut quibusdam maxime. Non enim perspiciatis.','assets/img/events-3.jpg','2023-04-28 12:35:43','2023-06-05 22:14:02','A');
 /*!40000 ALTER TABLE `tbeventos` ENABLE KEYS */;
 
 
@@ -2060,13 +2088,18 @@ CREATE TABLE `tbmenucardapio` (
   `alteracao` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `ativo` char(1) NOT NULL DEFAULT 'A',
   PRIMARY KEY (`idmenucardapio`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tbmenucardapio`
 --
 
 /*!40000 ALTER TABLE `tbmenucardapio` DISABLE KEYS */;
+INSERT INTO `tbmenucardapio` (`idmenucardapio`,`menucardapio`,`cadastro`,`alteracao`,`ativo`) VALUES 
+ (1,'Entradas','2023-04-28 12:35:43','2023-06-05 22:14:02','A'),
+ (2,'Café','2023-04-28 12:35:43','2023-06-05 22:14:02','A'),
+ (3,'Almoço','2023-04-28 12:35:43','2023-06-05 22:14:02','A'),
+ (4,'Jantar','2023-04-28 12:35:43','2023-06-05 22:14:02','A');
 /*!40000 ALTER TABLE `tbmenucardapio` ENABLE KEYS */;
 
 
@@ -2085,13 +2118,18 @@ CREATE TABLE `tbpessoas` (
   `alteracao` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `ativo` char(1) NOT NULL DEFAULT 'A',
   PRIMARY KEY (`idpessoas`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tbpessoas`
 --
 
 /*!40000 ALTER TABLE `tbpessoas` DISABLE KEYS */;
+INSERT INTO `tbpessoas` (`idpessoas`,`nome`,`email`,`telefone`,`cpf`,`cadastro`,`alteracao`,`ativo`) VALUES 
+ (1,'Saul Goodman','saulgoodman@gmail.com','(33) 9 1234-1234','123.123.123-12','2023-04-28 12:35:43','2023-06-05 22:14:02','A'),
+ (2,'Sara Wilsson','sarawilsson@gmail.com','(33) 9 1234-1234','123.123.123-12','2023-04-28 12:35:43','2023-06-05 22:14:02','A'),
+ (3,'Jena Karlis','jenakarlis@gmail.com','(33) 9 1234-1234','123.123.123-12','2023-04-28 12:35:43','2023-06-05 22:14:02','A'),
+ (4,'John Larson','johnlarson@gmail.com','(33) 9 1234-1234','123.123.123-12','2023-04-28 12:35:43','2023-06-05 22:14:02','A');
 /*!40000 ALTER TABLE `tbpessoas` ENABLE KEYS */;
 
 
@@ -2161,14 +2199,18 @@ CREATE TABLE `tbsobre` (
   `cadastro` datetime NOT NULL,
   `alteracao` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `ativo` char(1) NOT NULL DEFAULT 'A',
+  `imagem2` varchar(145) NOT NULL,
   PRIMARY KEY (`idsobre`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tbsobre`
 --
 
 /*!40000 ALTER TABLE `tbsobre` DISABLE KEYS */;
+INSERT INTO `tbsobre` (`idsobre`,`imagem`,`video`,`descricao`,`contmsg`,`contato`,`cadastro`,`alteracao`,`ativo`,`imagem2`) VALUES 
+ (1,'assets/img/about.jpg','https://www.youtube.com/watch?v=LXb3EKWsInQ','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore\r\n                magna aliqua.\r\n              </p>\r\n              <ul>\r\n                <li><i class=\"bi bi-check2-all\"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>\r\n                <li><i class=\"bi bi-check2-all\"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>\r\n                <li><i class=\"bi bi-check2-all\"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</li>\r\n              </ul>\r\n              <p>\r\n                Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate\r\n                velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident','Reserve uma mesa','+1 5589 55488 55','2023-05-23 23:45:23','2023-06-05 19:38:32','A','assets/img/about-2.jpg'),
+ (3,'./assets/img/testes/sobre/genshinp.png','https://www.youtube.com/watch?v=4oBpaBEMBIM','Genshin Impact é um jogo muito bom. Por favor jogue também. Por favor jogue também. Por favor jogue também. Por favor jogue também. Por favor jogue também. Por favor jogue também. Por favor jogue também. Por favor jogue também. Por favor jogue também. Por favor jogue também. Por favor jogue também. Por favor jogue também. Por favor jogue também. Por favor jogue também. Por favor jogue também. Por favor jogue também. Por favor jogue também. Por favor jogue também. Por favor jogue também. Por favor jogue também.','Jogue Agora','genshin.hoyoverse.com','2022-12-31 12:31:00','2023-06-05 19:38:32','D','./assets/img/testes/sobre/genshinv.jpg');
 /*!40000 ALTER TABLE `tbsobre` ENABLE KEYS */;
 
 
