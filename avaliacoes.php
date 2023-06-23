002fb8dd9fbc898789794988e7f9e338460c4b8e
felpstired <analucabbage@gmail.com>
2023-06-23 15:48:44 -0300

    Att. Avaliações - Nova function

--- avaliacoes.php
+++ avaliacoes.php
@@ -1,6 +1,6 @@
 <?php
 
-$listar = listarRegistros('idpessoas, nome', 'tbpessoas', 'A');
+$listar = listarRegistrosJoin('tbpessoas.nome, tbavaliacoes.comentario, tbavaliacoes.profissao, tbavaliacoes.estrelas, tbavaliacoes.imagem', 'tbpessoas', 'INNER', 'tbavaliacoes', 'idpessoas', 'A');
 
 ?>
 
@@ -21,64 +21,60 @@ $listar = listarRegistros('idpessoas, nome', 'tbpessoas', 'A');
           echo '<h6 class="text-center mt-5 p-3 bg-danger text-white">Não existe registros no banco!</h6>';
         } else {
           foreach ($listar as $itemLista) {
-            $idpessoa = $itemLista->idpessoas;
             $nome = $itemLista->nome;
+            $comentario = $itemLista->comentario;
+            $profissao = $itemLista->profissao;
+            $estrelas = $itemLista->estrelas;
+            $imagem = $itemLista->imagem;
 
-            $listarb = listarRegistrosPar('comentario, profissao, estrelas, imagem', 'tbavaliacoes', 'A', 'idpessoas', $idpessoa);
-            if ($listarb === false) {
-              echo '<h6 class="text-center mt-5 p-3 bg-danger text-white">Não existe registros no banco!</h6>';
-            } else {
-              foreach ($listarb as $itemListab) {
-                $comentario = $itemListab->comentario;
-                $profissao = $itemListab->profissao;
-                $estrelas = $itemListab->estrelas;
-                $imagem = $itemListab->imagem;
+            ?>
 
-                ?>
+            <div class="swiper-slide">
+              <div class="testimonial-item">
+                <div class="row gy-4 justify-content-center">
+                  <div class="col-lg-6">
+                    <div class="testimonial-content">
+                      <p>
+                        <i class="bi bi-quote quote-icon-left"></i>
+                        <?php echo $comentario; ?>
+                        <i class="bi bi-quote quote-icon-right"></i>
+                      </p>
+                      <h3>
+                        <?php echo $nome; ?>
+                      </h3>
+                      <h4>
+                        <?php echo $profissao; ?>
+                      </h4>
+                      <div class="stars">
+                        <?php
 
-                <div class="swiper-slide">
-                  <div class="testimonial-item">
-                    <div class="row gy-4 justify-content-center">
-                      <div class="col-lg-6">
-                        <div class="testimonial-content">
-                          <p>
-                            <i class="bi bi-quote quote-icon-left"></i>
-                            <?php echo $comentario; ?>
-                            <i class="bi bi-quote quote-icon-right"></i>
-                          </p>
-                          <h3>
-                            <?php echo $nome; ?>
-                          </h3>
-                          <h4>
-                            <?php echo $profissao; ?>
-                          </h4>
-                          <div class="stars">
-                            <?php
+                        for ($i = 0; $i < 5; $i++) {
+                          if ($estrelas > $i) {
+                            echo '<i class="bi bi-star-fill"></i>';
+                          } else {
+                            echo '<i class="bi bi-star"></i>';
+                          }
+                          ;
+                        }
+                        ;
 
-                            for ($i = 0; $i < 5; $i++) {
-                              if ($estrelas > $i) {
-                                echo '<i class="bi bi-star-fill"></i>';
-                              } else {
-                                echo '<i class="bi bi-star"></i>';
-                              }
-                            }
-
-                            ?>
-                          </div>
-                        </div>
-                      </div>
-                      <div class="col-lg-2 text-center">
-                        <img src="<?php echo $imagem; ?>" class="img-fluid testimonial-img" alt="">
+                        ?>
                       </div>
                     </div>
                   </div>
-                </div><!-- End testimonial item -->
+                  <div class="col-lg-2 text-center">
+                    <img src="<?php echo $imagem; ?>" class="img-fluid testimonial-img" alt="">
+                  </div>
+                </div>
+              </div>
+            </div><!-- End testimonial item -->
 
-                <?php
-              }
-            }
+            <?php
           }
+          ;
         }
+        ;
+
 
         ?>
 
--- func/functions.php
+++ func/functions.php
@@ -10,10 +10,10 @@ function listarRegistros($campos, $tabela, $ativo) {
             return $sqlLista->fetchAll(PDO::FETCH_OBJ);
         } else {
             return false;
-        }
+        };
     } catch (PDOException $e) {
         return 'Não foi possível acessar os dados. Erro: ' . $e->getMessage();
-    }
+    };
 };
 
 function listarRegistrosPar($campos, $tabela, $ativo, $param2, $valp2) {
@@ -27,10 +27,45 @@ function listarRegistrosPar($campos, $tabela, $ativo, $param2, $valp2) {
             return $sqlLista->fetchAll(PDO::FETCH_OBJ);
         } else {
             return false;
-        }
+        };
     } catch (PDOException $e) {
         return 'Não foi possível acessar os dados. Erro: ' . $e->getMessage();
-    }
+    };
 };
+
+function listarRegistrosJoin($campos, $tabela, $join, $tabela2, $id, $ativo) {
+    $conn = conectar();
+    try {
+        $sqlLista = $conn->prepare("SELECT $campos FROM $tabela $join JOIN $tabela2 ON $tabela.$id = $tabela2.$id AND $tabela.ativo = ? AND $tabela2.ativo = ?");
+        $sqlLista->bindValue(1, $ativo, PDO::PARAM_STR);
+        $sqlLista->bindValue(2, $ativo, PDO::PARAM_STR);
+        $sqlLista->execute();
+        if ($sqlLista->rowCount() > 0) {
+            return $sqlLista->fetchAll(PDO::FETCH_OBJ);
+        } else {
+            return false;
+        };
+    } catch (PDOException $e) {
+        return 'Não foi possível acessar os dados. Erro: ' . $e->getMessage();
+    };
+};
+
+// function listarRegistrosJoin2($campos, $tabela, $join, $tabela2, $id, $join2, $tabela3, $id2, $ativo) {
+//     $conn = conectar();
+//     try {
+//         $sqlLista = $conn->prepare("SELECT $campos FROM $tabela $join JOIN $tabela2 ON $tabela.$id = $tabela2.$id $join2 JOIN $tabela3 ON $tabela2.$id2 = $tabela3.$id2 AND $tabela.ativo = ? AND $tabela2.ativo = ? AND $tabela3.ativo = ?");
+//         $sqlLista->bindValue(1, $ativo, PDO::PARAM_STR);
+//         $sqlLista->bindValue(2, $ativo, PDO::PARAM_STR);
+//         $sqlLista->bindValue(3, $ativo, PDO::PARAM_STR);
+//         $sqlLista->execute();
+//         if ($sqlLista->rowCount() > 0) {
+//             return $sqlLista->fetchAll(PDO::FETCH_OBJ);
+//         } else {
+//             return false;
+//         };
+//     } catch (PDOException $e) {
+//         return 'Não foi possível acessar os dados. Erro: ' . $e->getMessage();
+//     };
+// };
 
 ?>
